bf9e93ab3191022e777770c2c32bf311cff6638a
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-05-25 09:33:28 +0800

    Added chatbot. The chatbot is unlinked. The model is not yet saved either

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, json
+from flask import Flask, json, request
 from flask_cors import CORS
-
+# from chatbot import chatbot
 
 app = Flask(__name__)
 CORS(app)
+
+# chatbot = chatbot.Chatbot()
+# chatbot.main()
 
 
 @app.route('/')
@@ -13,7 +16,26 @@ def hello_world():
 
 @app.route('/functional_chat')
 def functional_chat():
-    data = {'intent': 'sample intent',
+    data = {'intent': 'Hardcoded response',
+            'entities': [
+                {
+                    'type': 'name',
+                    'value': 'alice'
+                }
+            ]}
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/wacky_chat', methods=['POST'])
+def wacky_chat():
+    request_json = request.json
+    question = 'Hi' if request_json is None else request_json.get('question', 'Hi')
+    data = {'intent': 'testing', #chatbot.answer(question),
             'entities': [
                 {
                     'type': 'name',
