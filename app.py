382a65c9fad30de3ac19ddb73e8d077b8b4fc99b
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-07-17 22:49:38 +0800

    remove chatbot

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, json, request
 from flask_cors import CORS
-# from chatbot import chatbot
 
 app = Flask(__name__)
 CORS(app)
-
-# chatbot = chatbot.Chatbot()
-# chatbot.main()
 
 
 @app.route('/')
@@ -14,37 +10,8 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/functional_chat')
-def functional_chat():
-    data = {'intent': 'Hardcoded response',
-            'entities': [
-                {
-                    'type': 'name',
-                    'value': 'alice'
-                }
-            ]}
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+@app.route('/echo')
+def echo():
+    question = request.args['question']
+    return 'You have asked: ' + question
 
-
-@app.route('/wacky_chat', methods=['POST'])
-def wacky_chat():
-    request_json = request.json
-    question = 'Hi' if request_json is None else request_json.get('question', 'Hi')
-    data = {'intent': 'testing', #chatbot.answer(question),
-            'entities': [
-                {
-                    'type': 'name',
-                    'value': 'alice'
-                }
-            ]}
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
